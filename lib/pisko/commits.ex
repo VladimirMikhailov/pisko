754cffd43269f1d4ea5139915c9d48e1efe11e71
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2016-04-13 02:14:42 +0300

    Update pisko commits doc

--- lib/pisko/commits.ex
+++ lib/pisko/commits.ex
@@ -1,6 +1,13 @@
 defmodule Pisko.Commits do
   @moduledoc """
-    Retrieve github commits wrapper
+    Retrieve github commits.
+    Responds with a full commit information
+    including stats from single commit api end point
+    (https://developer.github.com/v3/repos/commits/#get-a-single-commit)
+
+    Examples:
+
+      iex> Pisko.Commits.list("awesome/repo", [since: "2016-03-14"])
   """
 
   import Tentacat, only: [get: 4, get: 2]
