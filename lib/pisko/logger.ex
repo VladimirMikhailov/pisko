6919b53d76a80d0e8f054363112436389865a405
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2016-04-14 02:38:52 +0300

    Add pisko logger to save commits info into ES

---
+++ lib/pisko/logger.ex
@@ -0,0 +1,26 @@
+defmodule Pisko.Logger do
+  @moduledoc """
+    Saves commits into elasticsearch
+
+    Examples:
+      iex> Pisko.Logger.log(commit)
+
+
+    If you like to run the process in non-blocking
+    manner you could use start_child wrap function
+    which involved to perform log as Task process
+
+      iex> Pisko.start_child(commit)
+  """
+
+  alias Pisko.Elastix.Commits, as: CommitsDocument
+
+  def log(commit) do
+    CommitsDocument.index(commit)
+  end
+
+  @doc "Wrapper for starting supervisioning child log process"
+  def start_child(commit) do
+    Task.Supervisor.start_child(Pisko.Log, fn -> Pisko.Logger.log(commit) end)
+  end
+end
---
+++ test/pisko/logger_test.exs
@@ -0,0 +1,13 @@
+defmodule Pisko.LoggerTest do
+  use ExUnit.Case, async: true
+  alias Pisko.Elastix.Commits, as: CommitsDocument
+
+  setup do
+    {:ok, %{commit: %{"sha" => "1", "info" => "info"}}}
+  end
+
+  test "log/1 saves commit in elasticsearch", %{commit: %{"sha" => sha }} = context do
+    Pisko.Logger.log(context.commit)
+    assert CommitsDocument.get(sha) == context.commit
+  end
+end
